8430f55496e0d8e05e1508ec99ab455363a1c674
Roberto Santiago <santiclinic@Robertos-Air.home>
2025-01-01 20:07:39 +0100

    last commit

--- movies/views.py
+++ movies/views.py
@@ -1,28 +1,34 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-
-data = {
-    'movies': [
-        {
-            'id': 5,
-            'title': 'Jaws',
-            'year': 1669
-        },
-        {
-            'id': 6,
-            'title': 'Sharknado',
-            'year': 1600
-        },
-        {
-            'id': 7,
-            'title': 'The Meg',
-            'year': 2000
-        }
-    ]
-}
+from .models import Movie
 
 def movies(request):
-    return render(request, 'movies/movies.html', data)
+    data = Movie.objects.all()
+    return render(request, 'movies/movies.html', {'movies': data})
 
 def home(request):
     return HttpResponse('Home page')
+
+def detail(request, id):
+    data = Movie.objects.get(pk=id)
+    return render(request, 'movies/detail.html', {'movie': data})
+
+def add(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        year = request.POST.get('year')
+
+        if title and year:
+            movie = Movie(title=title, year=year)
+            movie.save()
+            return HttpResponseRedirect('/movies')  # Redirect to movie list after saving
+
+    return render(request, 'movies/add.html')  # Display form if not POST
+
+def delete(request, id):
+    try:
+        movie = Movie.objects.get(pk=id)
+        movie.delete()  # Delete the movie
+    except Movie.DoesNotExist:
+        pass  # If the movie doesn't exist, do nothing
+    return HttpResponseRedirect('/movies')  # Redirect to movie list after deletion
